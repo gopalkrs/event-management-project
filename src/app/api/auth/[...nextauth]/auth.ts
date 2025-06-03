9a695b485f8f4e8d2a12f7139399ebe7ef3db342
Gopal <raunakchd1@gmail.com>
2025-06-03 21:04:35 +0530

    refactor(auth): streamline NextAuth configuration and enhance login button functionality

--- src/app/api/auth/[...nextauth]/auth.ts
+++ src/app/api/auth/[...nextauth]/auth.ts
@@ -1,17 +1,17 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
+import NextAuth from "next-auth";
 import Github from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 import { DrizzleAdapter } from '@auth/drizzle-adapter'
 import { db } from "@/lib/database/db";
 
-export const authConfig = {
+export const { handlers, auth, signIn, signOut } = NextAuth({
     providers: [
         Google({
             clientId: process.env.GOOGLE_CLIENT_ID || "",
             clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
-            httpOptions: {
-                timeout: 10000, // 10 seconds
-              },
+            // httpOptions: {
+            //     timeout: 10000, // 10 seconds
+            //   },
         }),
         // Github({
         //     clientId: process.env.GITHUB_CLIENT_ID || "",
@@ -23,20 +23,20 @@ export const authConfig = {
     session: {
         strategy: "database",
     },
-    callbacks: {
-        async signIn({ account }) {
-            if (account && account.providerAccountId) {
-              account.providerAccountId = String(account.providerAccountId); // 👈 Fix the type
-            }
-            return true;
-          },
+    // callbacks: {
+    //     async signIn({ account }) {
+    //         if (account && account.providerAccountId) {
+    //           account.providerAccountId = String(account.providerAccountId); // 👈 Fix the type
+    //         }
+    //         return true;
+    //       },
 
-        async session({session, user}){
-            session.user.id = user?.id;
-            return session;
-        }
-    },
-    pages: {
-        error: "/auth/error", // your custom error page
-    }
-} satisfies NextAuthOptions
+    //     async session({session, user}){
+    //         session.user.id = user?.id;
+    //         return session;
+    //     }
+    // },
+    // pages: {
+    //     error: "/auth/error", // your custom error page
+    // }
+})
--- src/lib/actions/auth.ts
+++ src/lib/actions/auth.ts
@@ -1,11 +1,11 @@
-// "use server"
+"use server"
 
-// import { signIn, signOut } from "@/app/api/auth/[...nextauth]/auth"
+import { signIn, signOut } from "@/app/api/auth/[...nextauth]/auth"
 
-// export const logIn = async () =>{
-//     await signIn("google", {redirectTo: "/"})
-// }
+export const logIn = async () =>{
+    await signIn("google", {redirectTo: "/"})
+}
 
-// export const logOut = async () =>{
-//     await signOut("google", {redirectTo: "/"})
-// }
+export const logOut = async () =>{
+    await signOut("google", {redirectTo: "/"})
+}
