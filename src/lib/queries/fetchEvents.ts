121a62f07c515e685e10a332c595d4491f8c75d8
Gopal <raunakchd1@gmail.com>
2025-06-25 14:28:36 +0530

    fix: update API endpoint URLs to remove localhost reference

--- src/lib/queries/fetchEvents.ts
+++ src/lib/queries/fetchEvents.ts
@@ -1,40 +1,40 @@
 import axios from "axios"
 
 export const fetchEventsFilter = () => {
-    const res = axios.get(`http://localhost:3000/api/events?eventType=concert,comedy`);
+    const res = axios.get(`/api/events?eventType=concert,comedy`);
     if(!res) throw new Error("Error while fetching events");
 
     return res;
 }
 
 export const fetchSportsFilter = () => {
-    const res = axios.get(`http://localhost:3000/api/events?eventType=sports`);
+    const res = axios.get(`/api/events?eventType=sports`);
     if(!res) throw new Error("Error while fetching events");
 
     return res;
 }
 export const fetchActivitiesFilter = () => {
-    const res = axios.get(`http://localhost:3000/api/events?eventType=activities`);
+    const res = axios.get(`/api/events?eventType=activities`);
     if(!res) throw new Error("Error while fetching events");
 
     return res;
 }
 export const fetchRecentEvents = () => {
-    const res = axios.get(`http://localhost:3000/api/events?recent=true`);
+    const res = axios.get(`/api/events?recent=true`);
     if(!res) throw new Error("Error while fetching events");
 
     return res;
 }
 
 export const fetchAllEvents = () => {
-    const res = axios.get(`http://localhost:3000/api/events`);
+    const res = axios.get(`/api/events`);
     if(!res) throw new Error("Error while fetching events");
 
     return res;
 }
 
 export const fetchSingleEvent = ({eventId} : {eventId : string}) => {
-    const res = axios.get(`http://localhost:3000/api/events/${eventId}`);
+    const res = axios.get(`/api/events/${eventId}`);
     if(!res) throw new Error("Error while fetching events");
 
     return res;
